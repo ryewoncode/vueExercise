53c65c7f7591cb77b89a199778eac6ccd7e9bbe1
ryewoncode <mykky0919@gmail.com>
2021-01-12 17:26:27 +0900

    vueExercise

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 10,
+            name: '',
+            confirmedName: ''
+        };
+    },
+    methods: {
+        comfirmInput() {
+            this.confirmedName = this.name;
+        },
+        submitForm() {
+            alert('Submitted!');
+        },
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName;
+        },
+        add(num) {
+            this.counter = this.counter + num;
+            // this.counter++;
+        },
+        reduce(num) {
+            this.counter = this.counter - num;
+            // this.counter--;
+        }
+    },
+});
+
+app.mount('#events');
